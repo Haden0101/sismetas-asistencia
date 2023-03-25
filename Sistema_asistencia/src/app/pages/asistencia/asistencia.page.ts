3db3533388886b413a2fbe5fe07805e5216c9b6f
Isaias Quiroz <isaias_quiroz_castillo@hotmail.com>
2023-03-25 02:04:22 -0500

    Se suben cambios para la plataforma de asistencias

--- Sistema_asistencia/src/app/pages/asistencia/asistencia.page.ts
+++ Sistema_asistencia/src/app/pages/asistencia/asistencia.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { InfiniteScrollCustomEvent, AlertController, IonicSafeString, IonInfiniteScroll, LoadingController} from '@ionic/angular';
+import { IonModal, AnimationController   } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-asistencia',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsistenciaPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonModal) modal!: IonModal;
+  @ViewChild(IonModal) modalEdit!: IonModal;
+  @ViewChild(IonInfiniteScroll) infiniteScroll!: IonInfiniteScroll;
+  items: string[] = [];
+
+  public name!: IonicSafeString;
+  public sec_name!: IonicSafeString;
+  public cel!: number;
+  public mail!: String;
+  public cump!: Date;
+  public message!: IonicSafeString;
+
+  handlerMessage = '';
+  roleMessage = '';
+
+  public ok: any = this.showLoading();
+
+
+  constructor(
+    private alertController: AlertController,
+    private loadingCtrl: LoadingController,
+    private animationCtrl: AnimationController)
+    { }
 
   ngOnInit() {
+    this.generateItems();
+    this.showLoading();
   }
+
+  /* Función para recargar la pagina */
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      duration: 3000,
+      spinner: 'circles',
+    });
+
+    loading.present();
+  }
+
+  /* Desde acá se inicia el listado de Integrantes >>> */
+
+  private generateItems() {
+    const count = this.items.length + 1;
+    for (let i = 0; i < 50; i++) {
+      this.items.push(`Item ${count + i}`);
+    }
+  }
+
+  onIonInfinite(ev:any) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
+
+/* FIN DE CODIGO */
 
 }
--- Sistema_asistencia/src/app/pages/dashboard/dashboard.page.ts
+++ Sistema_asistencia/src/app/pages/dashboard/dashboard.page.ts
@@ -16,11 +16,48 @@ export class DashboardPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.generarGrafico();
+    this.generarGraficoOne();
+    this.generarGraficoTwo();
   }
 
-  generarGrafico(){
-    const canva = document.getElementById('myChart');
+
+  /* Sección de Graficos para el dashBoard */
+  generarGraficoOne(){
+    const canva = document.getElementById('myChartOne');
+    const ctx: any = (canva as HTMLCanvasElement).getContext('2d');
+
+    const data = {
+      labels: [
+        'Enero','Febrero','Marzo','Abril','Mayo','Junio',
+        'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciemnbre'
+      ],
+      datasets: [{
+        label: 'Deshabilitar',
+        data: [300, 50, 100, 45, 23, 60, 23, 87, 123, 234, 56, 24],
+        backgroundColor: [
+          'rgb(255, 99, 132)','rgb(54, 162, 235)','rgb(255, 205, 86)',
+          'rgb(70, 193, 48)','rgb(136, 58, 193)','rgb(207, 211, 66)',
+          'rgb(100, 123, 88)','rgb(66, 218, 193)','rgb(218, 66, 102)'
+        ],
+        hoverOffset: 4
+      }]
+    };
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  generarGraficoTwo(){
+    const canva = document.getElementById('myChartTwo');
     const ctx: any = (canva as HTMLCanvasElement).getContext('2d');
 
     new Chart(ctx, {
